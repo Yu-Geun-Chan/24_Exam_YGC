f127504125f9f1adc219c886d5af4e6441b3264b
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-27 13:44:30 +0900

    No.1 question

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,8 +1,21 @@
 package org.koreait;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
+        for (int i = 1; i <= m; i++) {
+            if (i > 1) {
+                System.out.println();
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+        }
     }
 }
