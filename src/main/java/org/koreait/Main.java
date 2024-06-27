ac40ca75b019cdc6bc2a422656d553de8862a192
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-27 14:07:55 +0900

    No.3 question

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -4,18 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-
-        for (int i = 1; i <= m; i++) {
-            if (i > 1) {
-                System.out.println();
-            }
-            for (int j = 1; j <= n; j++) {
-                System.out.print("*");
-            }
-        }
+        int n = 9;
+        for (int i = 0; i <= n; i++) System.out.println(fibonacci(i));
+    }
+    private static int fibonacci(int num) {
+        if (num <= 1) return num;
+        return fibonacci(num - 1) + fibonacci(num - 2);
     }
 }
+
+
